c1210806ed2dd1d1010b403413800db91b9ddb11
beni69 <73666440+beni69@users.noreply.github.com>
2023-06-01 14:07:06 +0200

    init prez

--- flake.nix
+++ flake.nix
@@ -1,16 +1,29 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: {
-    devShells.default = with import nixpkgs { inherit system; }; mkShell {
-      buildInputs = [ prisma-engines openssl ];
-      shellHook = ''
-        export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
-        export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
-        export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-        export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
-        export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
-      '';
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-prez = {
+      url = "github:beni69/kareszklub-nix";
+      inputs = { nixpkgs.follows = "nixpkgs"; flake-utils.follows = "flake-utils"; };
     };
-  });
+  };
+
+  outputs = { self, nixpkgs, flake-utils, nix-prez }: flake-utils.lib.eachDefaultSystem (system: nixpkgs.lib.recursiveUpdate
+    {
+      devShells.default = with import nixpkgs { inherit system; }; mkShell {
+        buildInputs = [ prisma-engines openssl ];
+        shellHook = ''
+          export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+        '';
+      };
+    }
+    ((nixpkgs.legacyPackages.${system}.callPackage nix-prez.lib { }).mkFlake {
+      src = ./prez;
+      version = "0.0.0";
+      pname = "quiz-prez";
+      incremental = true;
+    }));
 }
